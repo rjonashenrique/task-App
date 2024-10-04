0982b6bca17bb14ce7751a97f6675809aa809518
JONAS HENRIQUE RODRIGUES DE SOUSA <rjonashenrique32@gmail.com>
2024-10-03 21:38:59 -0300

    javascript

--- js/srcipt.js
+++ js/srcipt.js
@@ -6,6 +6,11 @@ btnAdd.addEventListener('click',function(){
     addtask();
 })
 
+inputText.addEventListener('keydown' , function(e){
+   if(e.code == 'Enter')
+   addtask();
+})
+
 function addtask(){
      if (inputText.value == ''){
         alert("você precia adicionar a desrição da tarefa");
@@ -14,7 +19,33 @@ function addtask(){
         let li = document.createElement("li");
         li.innerHTML = inputText.value;
         taskLista.appendChild(li);
+
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
      }
 
      inputText.value = ' ';
+     savaData();
 }
+
+// * 03/10/2024
+
+taskLista.addEventListener('click', function(e){
+   if (e.target.tagName == 'LI'){
+      e.target.classList.toggle('checked');
+      savaData();
+   } else if (e.target.tagName == 'SPAN'){
+      e.target.parentElement.remove();
+      savaData();
+   }
+})
+
+function savaData(){
+   localStorage.setItem("task_data" , taskLista.innerHTML);
+}
+function loadData(){
+   taskLista.innerHTML = localStorage.getItem('task_data');
+}
+
+loadData();
